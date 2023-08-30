bd6944d082ae965c0dd4c800af1bce12b0215d79
Norwood Henderson <norwoodmhii@gmail.com>
2023-08-30 11:34:50 -0400

    conditionals intro work

---
+++ main.js
@@ -0,0 +1,71 @@
+// Check your math.
+
+/*
+if(1 + 1 > 5){
+  console.log("True fact!")
+}else {
+  console.log("LIES!")
+}
+*/
+
+/*
+if(4 + 5 <= 20){
+  console.log("True fact!")
+}else {
+  console.log("LIES!")
+}
+*/
+
+/*
+if(6 + 2 >= 0){
+  console.log("True fact!")
+}else {
+  console.log("LIES!")
+}
+*/
+
+// Dog people vs cat people
+
+/*
+let likesDogs = true;
+
+if(likesDogs = true){
+  console.log("You're a great person!")
+}else {
+  console.log("You're a cat person!")
+}
+*/
+
+// Greetings
+/*
+const firstName = "Norwood";
+let prefLanguage = "English";
+
+if (preLanguage = "English"){
+  console.log(`Hello, ${firstName}!`)
+  else if(preLanguage = "Spanish"){
+    console.log(`Hola,$firstName}!`){
+      else if(preLanguage = "French"){
+        console.log(`Bonjour,${firstName}!`)
+      }
+    }
+  }
+}
+*/
+
+// student grades
+
+
+let grade = 86
+
+if(grade >= 0 && grade < 69){
+  console.log("You got a F");
+} else if(grade >= 70 && grade <76){
+  console.log("You got a D"); 
+} else  if(grade >=77 && grade <84 ){
+  console.log("You got a C");
+}else if(grade >= 85 && grade <92){
+  console.log("You got an B");
+} else if(grade >= 93 && grade <=100){
+  console.log("You got an A")
+}
